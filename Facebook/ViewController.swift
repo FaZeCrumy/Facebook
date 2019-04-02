62760c59330e0f0213001cf8fe678c28682b64e6
Nicholas Wong <nwong2022@steam-8.local>
2019-04-02 10:11:06 -0700

    test

--- Facebook/ViewController.swift
+++ Facebook/ViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.blue
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func goodBye(_ sender: Any) {
+        print("say goodbye to you personal info!")
+    }
+    
 
 }
 
